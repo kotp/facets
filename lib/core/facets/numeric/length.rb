bad977333d9deafd5baa31967dac18a530e13a02
Victor Goff <keeperotphones@gmail.com>
2010-02-11 02:00:37 -0600

    Had to restore an unintentinal change during the Christmas Even Audit.

--- lib/core/facets/numeric/length.rb
+++ lib/core/facets/numeric/length.rb
@@ -1,9 +1,23 @@
+=begin
+Return the number of digits in an Numeric.
+
+ 1.length   #=> 1
+
+ 10.length  #=> 2
+
+ 100.length #=> 3
+
+CREDIT: Victor H. Goff III
+
+=end
+
+
 class Numeric
 
-  # Returns +self+.
+  # Returns the number of digits as an Integer.
 
   def length
-    self
+    self.to_s.length
   end
 
 end
--- test/core/integer/test_length.rb
+++ test/core/integer/test_length.rb
@@ -1,11 +1,11 @@
-require 'facets/integer/length'
+require 'facets'
 require 'test/unit'
 
 
 class TC_Integer < Test::Unit::TestCase
   def test_length
     (0..10).each do |power|
-      assert((10 ** power).length == (power + 1))
+      assert((10 ** power).length == (power + 1),"#{(10**power)} has #{(10**power).to_s.length} digits, but Facets::length currently reports #{(10 ** power).length}")
     end
   end
 end
